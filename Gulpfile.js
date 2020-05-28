dbe012694338eb20c46a25335609407f1b610f20
miherlosev <miherlosev@mail.ru>
2020-05-28 17:50:01 +0300

    initial

---
+++ Gulpfile.js
@@ -0,0 +1,24 @@
+const gulp            = require('gulp');
+const eslint          = require('gulp-eslint');
+const mocha           = require('gulp-mocha-simple');
+const SpecWithRetries = require('./src/index');
+
+gulp.task('lint', () => {
+    return gulp
+        .src([
+            'src/**/*.js',
+            'Gulpfile.js'
+        ])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task('preview', () => {
+    return gulp
+        .src('test/*.js')
+        .pipe(mocha({
+            reporter: SpecWithRetries,
+            timeout:  typeof v8debug === 'undefined' ? 2000 : Infinity // NOTE: disable timeouts in debug
+        }));
+});
---
+++ test/suite2.js
@@ -0,0 +1,36 @@
+const assert = require('assert');
+
+describe('Test suite 2', function () {
+    let unstable1RunCount = 0;
+    let unstable2RunCount = 0;
+
+    this.retries(3);
+
+    it('Passed', () => {
+        assert.ok(true);
+    });
+
+    it('Failed', () => {
+        assert.ok(false);
+    });
+
+    it('Pending');
+
+    it('Unstable - 1', () => {
+        unstable1RunCount++;
+
+        if(unstable1RunCount === 2)
+            assert.ok(true);
+        else
+            assert.ok(false);
+    });
+
+    it('Unstable - 2', () => {
+        unstable2RunCount++;
+
+        if(unstable2RunCount === 2)
+            assert.ok(true);
+        else
+            assert.ok(false);
+    });
+});
